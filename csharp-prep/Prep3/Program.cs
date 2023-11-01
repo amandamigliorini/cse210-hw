99e93954e43e45d3b5e524340cdb07db3513b553
amandamigliorini <amanda.migliorini@hotmail.com>
2023-11-01 17:08:15 -0300

    Prep 3 activity done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+
+        string keepGaming = "yes";
+
+        do {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int guess;
+        int guesses = 0;
+
+           do{
+            Console.Write("What is your guess? ");
+            string sGuess = Console.ReadLine();
+            guess = int.Parse(sGuess);
+            guesses ++;
+            if (guess < magicNumber) {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber){
+                Console.WriteLine("Lower");
+            }
+            else if (guess == magicNumber){
+                Console.WriteLine ($"You guessed it! And you guessed it in {guesses} guesses!");
+                Console.Write("Do you want to play it again? (yes/no) ");
+                keepGaming = Console.ReadLine();
+            }
+        }while (guess != magicNumber);
+ 
+        }while (keepGaming == "yes");
+
+        Console.WriteLine ("Thank you!");
+        
+    } 
+            
+    
 }
