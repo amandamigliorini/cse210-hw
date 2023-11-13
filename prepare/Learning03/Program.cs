eacb943f8d8364d415ceb48b3f8b2c5bcf9d744f
amandamigliorini <amanda.migliorini@hotmail.com>
2023-11-13 18:42:08 -0300

    Learning 03 Done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        Fraction f2 = new Fraction(5);
+        Fraction f3 = new Fraction (3,4);
+        Fraction f4 = new Fraction (1,3);
+
+        f1.SetTop(1);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetTop());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetTop());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+        
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+        
+   
     }
+
 }
